0eff7e053e10728bdc309b198fcba1872720195b
hmalinowsk <hmalinowski@realogicworks.com>
2023-06-01 14:57:34 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import logo from "./logo.svg";
 import "@aws-amplify/ui-react/styles.css";
@@ -24,16 +23,3 @@ function App({ signOut }) {
 }
 
 export default withAuthenticator(App);
-
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1>Hello from V2</h1>
-            </header>
-        </div>
-    );
-}
-
-export default App;
