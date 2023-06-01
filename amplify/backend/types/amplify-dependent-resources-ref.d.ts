aeaa5507a1584aae226dc3ecc14338b2729c3094
hmalinowsk <hmalinowski@realogicworks.com>
2023-06-01 15:35:08 -0400

    Add API

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyapp72c66a8b": {
       "AppClientID": "string",
